14d189d06787d39c791fdf5e56519ce5da136999
Jardson Alan <jardsonalan132@gmail.com>
2025-03-22 19:51:05 -0300

    feat: subindo a base do projeto

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <v-app>
+    <!-- Barra de navegação -->
+    
+    <v-main>
+      <!-- Conteúdo principal -->
+    </v-main>
+
+    <v-footer>
+      <!-- Rodapé -->
+    </v-footer>
+  </v-app>
+</template>
+
+<style scoped>
+</style>
