e3efd4ff5a8085313cf71e63506b6812bfaf39bd
Jardson Alan <jardsonalan132@gmail.com>
2025-03-29 17:52:42 -0300

    feat: adicionando sistema para cadastrar e remover dados dos clientes na tabela; sistema para verificar se o serviço foi realizado e incrementar valor do serviço específico no saldo da conta; propriedade computada para verificar o total de clientes que estão cadastrados na tabela

--- src/views/DashboardScreen.vue
+++ src/views/DashboardScreen.vue
@@ -1,13 +1,79 @@
 <script setup lang="ts">
 import NavigationDrawerDashboard from '@/components/NavigationDrawerDashboard.vue';
-import { ref } from 'vue';
+import { ref, computed } from 'vue';
+
+type Client = {
+  id: number,
+  name: string,
+  phone: string,
+  hours: string,
+  service: string
+}
 
 const viewMoney = ref(false)
-
 const isDialogOpen = ref(false)
 
+const name = ref('')
+const phone = ref('')
+const hours = ref('')
+const service = ref('')
+// Armazena o valor do serviço, quando o usuário confirma o atendimento na função (checkClient)
+const money = ref(0)
+// Armazena o total de clientes presentes na tabela
+const countClients = computed(() => listClients.value.length)
+// Serve para incrementar o id
+let increment = 0
+
+const listClients = ref<Client[]>([])
 const listHours = ['08:00', '08:30', '09:00', '09:30', '10:00', '10:30', '11:00', '11:30', '12:00']
 const listServices = ['Cabelo', 'Barba', 'Cabelo + Barba']
+
+// Função para cadastrar os dados dos clientes na tabela
+function registerClient () {
+  if ((name.value && phone.value && hours.value && service.value) !== ''){
+    listClients.value.push({
+      id: increment,
+      name: name.value,
+      phone: phone.value,
+      hours: hours.value,
+      service: service.value
+    })
+
+    name.value = ''
+    phone.value = ''
+    hours.value = ''
+    service.value = ''
+    increment++
+  }
+}
+
+// Função para deletar um cliente cadastrado na lista
+function deleteClient(id: number) {
+  listClients.value = listClients.value.filter(i => i.id != id)
+}
+
+// Função para indicar que o atendimento foi feito
+function checkClient(id: number) {
+  verifyValueService(id)
+  listClients.value = listClients.value.filter(i => i.id != id)
+}
+
+// Função para verificar o tipo do serviço fornecido e adicionar o preço na variável (money)
+function verifyValueService(id: number) {
+  listClients.value.filter((item) => {
+    // Verifica o tipo do serviço para adicionar o valor
+    if (item.service === 'Cabelo' && item.id === id) {
+      // Se for corte de cabelo, adiciona R$30,00 ao saldo
+      money.value += 30
+    } else if (item.service === 'Barba' && item.id === id) {
+      // Se for corte da barba, adiciona R$20,00 ao saldo
+      money.value += 20
+    } else if (item.service === 'Cabelo + Barba' && item.id === id) {
+      // Se for os dois cortes, adiciona R$50,00 ao saldo
+      money.value += 50
+    }
+  })
+}
 </script>
 
 <template>
@@ -26,13 +92,13 @@ const listServices = ['Cabelo', 'Barba', 'Cabelo + Barba']
                 <v-card-item title="Receita" prepend-icon="mdi-currency-usd" class="pt-0"/>
                 <v-btn :icon="viewMoney?'mdi-eye':'mdi-eye-off'" variant="text" @click.stop="viewMoney = !viewMoney"/>
               </div>
-              <v-card-text class="text-h5">R$ {{ viewMoney?'...':0 }}</v-card-text>
+              <v-card-text class="text-h5">R$ {{ viewMoney?'...':money }}</v-card-text>
             </v-card>
           </v-col>
           <v-col cols="12" md="6">
             <v-card class="mx-auto pa-3" height="150">
               <v-card-item title="Clientes" prepend-icon="mdi-account-group"/>
-              <v-card-text class="text-h5 pt-4">0</v-card-text>
+              <v-card-text class="text-h5 pt-4">{{ countClients }}</v-card-text>
             </v-card>
           </v-col>
           <v-col cols="12">
@@ -57,20 +123,20 @@ const listServices = ['Cabelo', 'Barba', 'Cabelo + Barba']
                       <v-card-text>
                         <v-row>
                           <v-col cols="6">
-                            <v-text-field label="Nome" variant="outlined"/>
+                            <v-text-field label="Nome" variant="outlined" v-model="name"/>
                           </v-col>
                           <v-col cols="6">
-                            <v-text-field label="Telefone" placeholder="(00) 00000-0000" variant="outlined"/>
+                            <v-text-field label="Telefone" placeholder="(00) 00000-0000" variant="outlined" v-model="phone"/>
                           </v-col>
                         </v-row>
-                        <v-select label="Horário" :items="listHours" variant="outlined"/>
-                        <v-select label="Serviço" :items="listServices" variant="outlined"/>
+                        <v-select label="Horário" :items="listHours" variant="outlined" v-model="hours"/>
+                        <v-select label="Serviço" :items="listServices" variant="outlined" v-model="service"/>
                       </v-card-text>
 
                       <v-card-actions>
                         <v-spacer/>
                         <v-btn variant="text" @click="isDialogOpen = false">Cancelar</v-btn>
-                        <v-btn variant="tonal" color="success">Adicionar</v-btn>
+                        <v-btn variant="tonal" color="success" @click="registerClient">Adicionar</v-btn>
                       </v-card-actions>
                     </v-card>
                   </v-dialog>
@@ -80,21 +146,22 @@ const listServices = ['Cabelo', 'Barba', 'Cabelo + Barba']
               <v-table>
                 <thead>
                   <tr>
-                    <th>Nome</th>
-                    <th>Telefone</th>
-                    <th>Horário</th>
-                    <th>Serviço</th>
+                    <th class="font-weight-bold">Nome</th>
+                    <th class="font-weight-bold">Telefone</th>
+                    <th class="font-weight-bold">Horário</th>
+                    <th class="font-weight-bold">Serviço</th>
                     <th></th>
                   </tr>
                 </thead>
                 <tbody>
-                  <tr>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td class="text-end">
-                      <v-btn icon="mdi-delete" variant="text" class="text-red" density="comfortable"/>
+                  <tr v-for="(client, index) in listClients" :key="index">
+                    <td>{{ client.name }}</td>
+                    <td>{{ client.phone }}</td>
+                    <td>{{ client.hours }}</td>
+                    <td>{{ client.service }}</td>
+                    <td class="text-end" v-if="listClients.length > 0">
+                      <v-btn icon="mdi-check-bold" variant="text" class="text-green mr-3" density="comfortable" @click="checkClient(client.id)"/>
+                      <v-btn icon="mdi-delete" variant="text" class="text-red" density="comfortable" @click="deleteClient(client.id)"/>
                     </td>
                   </tr>
                 </tbody>
